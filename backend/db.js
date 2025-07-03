f2b86f185c2a26d0399c0f6488ef801aafd1a112
Sarnitha A D <sharnithadhandapani@gmail.com>
2025-07-03 13:11:12 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,15 @@
-// db.js
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: { rejectUnauthorized: true },
+  ssl: { rejectUnauthorized: true }, // Aiven requires SSL
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0// Aiven requires SSL by default
+  queueLimit: 0
 });
 
 module.exports = pool;
