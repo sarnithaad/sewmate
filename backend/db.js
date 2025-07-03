6af0708af49d269b9e0d3b783535ff15ad359015
Sarnitha A D <sharnithadhandapani@gmail.com>
2025-07-03 12:53:06 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,13 +2,15 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "Sharnitha@2003",
-  database: process.env.DB_NAME || "sewmate",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: true };
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0// Aiven requires SSL by default
 });
 
 module.exports = pool;
