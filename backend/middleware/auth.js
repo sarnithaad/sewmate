cfe314904dbf43bd493f5312708b2f3fc6e62005
Sarnitha A D <sharnithadhandapani@gmail.com>
2025-07-03 11:30:59 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret";
+const JWT_SECRET = process.env.JWT_SECRET || "Sharnitha@2003";
 
 function authenticate(req, res, next) {
   const auth = req.headers.authorization;
