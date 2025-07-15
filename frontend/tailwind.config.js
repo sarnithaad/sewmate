ae05a69cbed7fd138854d6684a74519e8e2d9e6b
Sarnitha A D <sharnithadhandapani@gmail.com>
2025-07-15 18:08:05 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  theme: { extend: {colors: {
+        beige: '#EED8B4',
+        lighttan: '#D2B48C',
+        brown: '#A67B5B',
+        darkbrown: '#5C4033',
+        richbrown: '#3B2A1A',
+      },
+   } },
   plugins: [],
+  
 };
